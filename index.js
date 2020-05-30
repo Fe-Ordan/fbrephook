fd65a952d60312107587415d4b6a394cc13e48be
Fe-Ordan <Fe-Ordan@users.noreply.github.com>
2020-05-30 17:21:47 +0100

    wxc

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	console.log(req.body)
-	// let messaging_events = req.body.entry[0].messaging
+	
+	 let messaging_events = req.body.entry[0].messaging
+	 console.log(messaging_events)
 	// for (let i = 0; i < messaging_events.length; i++) {
 	// 	let event = req.body.entry[0].messaging[i]
 	// 	let sender = event.sender.id
